263dad20223ccc8571d68e64613fbcca74f77012
MoveToEx <39954725+MoveToEx@users.noreply.github.com>
2026-04-12 18:52:33 +0800

    feat: Add auth requirement in RequireKeys component

--- frontend/src/components/require-umk.tsx
+++ frontend/src/components/require-umk.tsx
@@ -3,20 +3,26 @@ import { useEffect } from "react";
 import { AlertDialog as BaseAlertDialog } from "@base-ui/react";
 import UnlockDialog from "./dialogs/unlock";
 import useAuth from "@/hooks/use-auth";
+import { useNavigate } from "react-router";
 
 const handle = BaseAlertDialog.createHandle<undefined>();
 
 export default function RequireKeys() {
   const keys = useAppSelector((state) => state.key.value);
   const { data, isLoading } = useAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (isLoading || !data) return;
+    if (isLoading || data === undefined) return;
+
+    if (data === null) {
+      navigate('/');
+    }
 
     if (!keys) {
       handle.open(null);
     }
-  }, [keys, data, isLoading]);
+  }, [keys, data, isLoading, navigate]);
 
   return <UnlockDialog handle={handle} />;
 }
