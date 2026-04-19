0c227dff1d58f30f8232db050e1e81c5372ae252
MoveToEx <39954725+MoveToEx@users.noreply.github.com>
2026-04-19 20:45:55 +0800

    misc: Add i18n for drag & drop component

--- frontend/src/components/drag-drop.tsx
+++ frontend/src/components/drag-drop.tsx
@@ -1,11 +1,12 @@
-import { useState, useRef } from 'react';
-import _ from 'lodash';
-import { Upload } from 'lucide-react';
-import { Button } from './ui/button';
-import { cn } from '@/lib/utils';
+import { useState, useRef } from "react";
+import _ from "lodash";
+import { Upload } from "lucide-react";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { useTranslation } from "react-i18next";
 
 export default function DragDrop({
-  className = '',
+  className = "",
   multiple = false,
   onChange,
   ...rest
@@ -14,6 +15,7 @@ export default function DragDrop({
   multiple?: boolean,
   onChange: (files: File[]) => void
 }) {
+  const { t } = useTranslation();
   const [dragging, setDragging] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -22,8 +24,10 @@ export default function DragDrop({
       {...rest}
       className={
         cn(
-          'flex flex-col items-center justify-center rounded border-2 border-dashed border-gray-400 dark:border-gray-700 w-full min-h-48',
-          dragging ? '**:pointer-events-none bg-gray-200 dark:bg-gray-800 border-blue-400 dark:border-blue-900' : '',
+          "flex flex-col items-center justify-center rounded border-2 border-dashed border-gray-400 dark:border-gray-700 w-full min-h-48",
+          dragging
+            ? "**:pointer-events-none bg-gray-200 dark:bg-gray-800 border-blue-400 dark:border-blue-900"
+            : "",
           className,
         )
       }
@@ -47,8 +51,8 @@ export default function DragDrop({
         e.stopPropagation();
         setDragging(false);
 
-        const items = [...e.dataTransfer.items].filter(item => {
-          return item.kind === 'file';
+        const items = [...e.dataTransfer.items].filter((item) => {
+          return item.kind === "file";
         });
 
         const result = _.compact(items.map(item => item.getAsFile()));
@@ -56,27 +60,31 @@ export default function DragDrop({
         if (result.length > 0) {
           onChange(result);
         }
-      }}>
+      }}
+    >
       <input
         ref={inputRef}
-        className='hidden'
-        type='file'
+        className="hidden"
+        type="file"
         onChange={(e) => {
           if (e.target.files) onChange([...e.target.files]);
         }}
-        multiple={multiple} />
+        multiple={multiple}
+      />
       <Upload size={48} />
-      <span className='text-md text-center'>
-        Drop files here or&nbsp;
+      <span className="text-md text-center">
+        {t("dragDrop.dropPrompt")}
         <Button
-          variant='link'
-          className='p-0 text-secondary-foreground'
+          type="button"
+          variant="link"
+          className="p-0 text-secondary-foreground"
           onClick={() => {
             if (inputRef.current) {
               inputRef.current.click();
             }
-          }}>
-          browse
+          }}
+        >
+          {t("dragDrop.browse")}
         </Button>
       </span>
     </div>
